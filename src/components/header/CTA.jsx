47a3795836e14efce1585fe1df85111c5d331165
Adarsh Alone <anitaalone1974@gmail.com>
2023-08-04 15:04:31 +0530

    first commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     
     <div className="cta">
-        <a href="C:\Users\Dell\Desktop\portfolio-website\src\assets\adarsh alone.pdf" className='btn'>Download CV</a>
+        <a href="/" className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact</a>
     </div>
 
